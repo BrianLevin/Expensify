cfc1cd3f23e9377f042b5afe73112bcbc727a751
Brian Levin <bml201095@gmail.com>
2020-08-27 20:57:34 -0400

    styling expenses

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,11 +1,15 @@
-
 import React from 'react';
 import { connect } from 'react-redux';
 import ExpenseListItem from './ExpenseListItem';
 import selectExpenses from '../selectors/expenses';
 
 export const ExpenseList = (props) => (
-  <div>
+  <div className="content-container">
+    <div className="list-header">
+      <div className="show-for-mobile">Expenses</div>
+      <div className="show-for-desktop">Expense</div>
+      <div className="show-for-desktop">Amount</div>
+    </div>
     {
       props.expenses.length === 0 ? (
         <p>No expenses</p>
